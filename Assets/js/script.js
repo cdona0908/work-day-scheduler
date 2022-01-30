26c14041d1d1661c2bb3e8058acfecccee39968b
Celia <celiamdona@gmail.com>
2022-01-29 20:33:39 -0500

    timeblock color coded

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,3 +2,32 @@
 //Show current date at the top
 var today = moment().format("dddd LL");
 $("#currentDay").append(today);
+
+//Evaluate if timeblock is in past, present or future
+
+var evalTimeBlock = function(){
+    var actualHour = moment().hour();
+    console.log(actualHour);
+    $(".time-block").each(function(){
+        var blockHour =$(this).attr("id");
+        console.log(blockHour);
+        
+        if (blockHour === actualHour){
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        } else if (blockHour < actualHour){
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+
+    });
+
+};
+
+evalTimeBlock();
